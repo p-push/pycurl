a104f656a9fba7f9e349aabc362a117de9533789
Oleg Pudeyev <oleg@bsdpower.com>
2013-12-17 13:03:52 -0800

    Start building pycurl

--- winbuild.py
+++ winbuild.py
@@ -2,8 +2,9 @@ root = 'c:/dev/build-pycurl'
 # where msysgit is installed
 git_root = 'c:/program files/git'
 libcurl_version = '7.34.0'
+pycurl_version = '7.19.0.2'
 
-import os, os.path, sys, subprocess, shutil
+import os, os.path, sys, subprocess, shutil, contextlib
 
 archives_path = os.path.join(root, 'archives')
 git_bin_path = os.path.join(git_root, 'bin')
@@ -29,17 +30,33 @@ def fetch(url, archive=None):
                 f.write(chunk)
         os.rename('.tmp.%s' % archive, archive)
 
+@contextlib.contextmanager
+def in_dir(dir):
+	old_cwd = os.getcwd()
+	try:
+		os.chdir(dir)
+		yield
+	finally:
+		os.chdir(old_cwd)
+
 def work():
 	os.environ['PATH'] += ";%s" % git_bin_path
 	if not os.path.exists(archives_path):
 		os.makedirs(archives_path)
-	os.chdir(archives_path)
-	fetch('http://curl.haxx.se/download/curl-%s.tar.gz' % libcurl_version)
-	if os.path.exists('curl-%s' % libcurl_version):
-		shutil.rmtree('curl-%s' % libcurl_version)
-	subprocess.check_call([tar_path, 'xf', 'curl-%s.tar.gz' % libcurl_version])
-	os.chdir(os.path.join('curl-%s' % libcurl_version, 'winbuild'))
-	subprocess.check_call(['nmake', '/f', 'Makefile.vc', 'mode=static', 'ENABLE_IDN=no'])
-	subprocess.check_call(['nmake', '/f', 'Makefile.vc', 'mode=dll', 'ENABLE_IDN=no'])
+	with in_dir(archives_path):
+		fetch('http://curl.haxx.se/download/curl-%s.tar.gz' % libcurl_version)
+		if os.path.exists('curl-%s' % libcurl_version):
+			shutil.rmtree('curl-%s' % libcurl_version)
+		subprocess.check_call([tar_path, 'xf', 'curl-%s.tar.gz' % libcurl_version])
+		with in_dir(os.path.join('curl-%s' % libcurl_version, 'winbuild')):
+			subprocess.check_call(['nmake', '/f', 'Makefile.vc', 'mode=static', 'ENABLE_IDN=no'])
+			subprocess.check_call(['nmake', '/f', 'Makefile.vc', 'mode=dll', 'ENABLE_IDN=no'])
+	
+		fetch('http://pycurl.sourceforge.net/download/pycurl-%s.tar.gz' % pycurl_version)
+		if os.path.exists('pycurl-%s' % pycurl_version):
+			shutil.rmtree('pycurl-%s' % pycurl_version)
+		subprocess.check_call([tar_path, 'xf', 'pycurl-%s.tar.gz' % pycurl_version])
+		with in_dir(os.path.join('pycurl-%s' % pycurl_version)):
+			subprocess.check_call([python_path, 'setup.py', 'build'])
 
 work()
